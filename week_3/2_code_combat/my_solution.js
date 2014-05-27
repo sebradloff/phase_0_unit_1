79bff5989d1653943ed23045cdb85501700d18f4
Sebastian Radloff <sradloff23@gmail.com>
2014-05-27 10:08:11 -0500

    Added a reflection, using git from command line to push

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -4,13 +4,106 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// Rescue Mission
+
+this.moveDown();//move down
+this.moveDown();//move down
+this.moveRight(); // move right
+this.moveUp();// move up
+this.moveUp();// move up
+this.moveRight();// move right
+this.moveRight();// move right
+this.moveDown();// move down
+this.attackNearbyEnemy();// attack
+
+//Grab the Mushroom
+
+// move up
+// move right
+// move left
+// move up
+// move up
+// attack
  
+
+// Drink Me
+
+// moveRight
+// attackNearbyEnemy
+// moveRight
+// moveDown
+// moveUp
+// moveRight
+// attackNearbyEnemy
  
- 
- 
- 
+ // Taunt the Guards
+
+ // moveRight x2
+ // moveUp x2
+ // moveRight 
+ // say to distract ogres
+ // attack X2
+ // say follow
+ // move right x2
+
+ // It's a trap
+
+ // move down x2
+ // say distract
+ // moveUp x2
+ // say attack
+
+// Taunt
+
+// say diff stuff x4
+
+// Cowardly Taunt
+
+// moveXY
+// say
+// moveXY
+
+// Commanding followers
+
+// say follow
+// move near ogres
+// say attack
+
+// Mobile Artillery
+
+// move artilery
+// fire on ogres x4
+
+
+/* 
+What is this referring to? Think programming-wise rather than in the terms of the game.
+	* the "this" is referring to your object, aka Thorin. The "commands" you are giving him are actually functions, and the this. 
+	format is used to call functions on certain objects.
+What does the () do in JavaScript?
+	* the "()" is where you place your arguments for whatever function you are calling.
+What is the point of the semicolons?
+	* the semicolons, is used to seperate statements either on the same line but can be optional if seperated by a line break.
+
  
  
  
 // Reflection:
+
+What parts of your strategy worked? What problems did you face?
+	* I thought code combat was straight forward so I didn't really have a strategy besides follow the guidelines. Only problems
+	I faced were mildly related to the interphase of code combat, or else I was fine.
+What questions did you have while coding? What resources did you find to help you answer them?
+	* No questions while coding.
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+	* None
+Did you learn any new skills or tricks?
+	* Going through the motions of js syntax was helpful as a refresher
+How confident are you with each of the Learning Competencies?
+	* I am confident with each of the learning competencies to the fullest extent.
+Which parts of the challenge did you enjoy?
+	* N/A
+Which parts of the challenge did you find tedious?
+	* I felt I just wanted to get through it. This should be reserved for someone who has never dealt with any programming before
+	because it's just a long excercise in syntax essentially. I understand why this would benefit someone who is new to programming 
+	immensly, but for me that was not the case. I can appreciate the excercise for what it is.
 // Write your reflection here.
