cd11949d14969c2d5748c2844e67fdcf30f391cd
Sebastian Radloff <sradloff23@gmail.com>
2014-05-28 10:37:41 -0500

    added person number and extra comment

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -1,4 +1,4 @@
-// My role in the group is: Person 0
+// My role in the group is: Person 1
 
 // Here is my part of the challenge:
 
@@ -6,3 +6,5 @@ I want to be able to do the following things with a group of numbers, even or od
 	1) I want to be able to sum all the numbers in a group.
 	2) I want to be able to calculate the mean of a group of numbers.
 	3) I want to be able to calculate the median of a group of numbers.
+
+You should come up with a way to test each of these abilities, making sure they work.
