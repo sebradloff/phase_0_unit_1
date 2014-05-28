9f4981f80d2dbc9c8a816e4ec7bc9b4f0f81597c
Sebastian Radloff <sradloff23@gmail.com>
2014-05-28 10:37:23 -0500

    Added Reflection

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,8 +1,8 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Sebastian Radloff
+//  2. George Zeng
 
 
 // 0. "YOU SIGNED... WHO?!"
@@ -43,3 +43,20 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 
 
 //  6.  Your Reflection:
+* What parts of your strategy worked? What problems did you face?
+	* Roadmapping where we were going and what we wanted to do helped us a lot. I was able to navigate properly when we both were on the 
+	same page, and he was able to write exactly what we had talked about. This also worked once we flipped roles.    
+* What questions did you have while coding? What resources did you find to help you answer them?
+	* General syntax questions regarding object creation. Emily helped us out a bit and we used the intro JavaScript docs on DBC github.  
+* What concepts are you having trouble with, or did you just figure something out? If so, what?  
+	* No troubles as of now. Object creation syntax, but I understand everything completed in this GPS session.
+* Did you learn any new skills or tricks?
+	* learned about utilizing a function call witin a  dimension of an object which was really cool.
+* How confident are you with each of the Learning Competencies? 
+	* I feel confident with all the Learning Competencies. Creating and manipulating objects is easy, using functions are no problem,
+	and I feel less confident debugging with Node, since I use the console, but I am doing fine so far.
+* Which parts of the challenge did you enjoy?
+	* I enjoyed all of it. It was a fun puzzle to solve with my partner.
+* Which parts of the challenge did you find tedious?
+	* N/A
+
