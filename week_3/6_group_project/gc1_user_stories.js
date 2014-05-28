945cead3332d58aaaa37dbaaba95bb0ef3cc18e3
Sebastian Radloff <sradloff23@gmail.com>
2014-05-28 08:17:01 -0500

    I am person 0

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,8 @@
+// My role in the group is: Person 0
+
+// Here is my part of the challenge:
+
+I want to be able to do the following things with a group of numbers, even or odd number of numbers. 
+	1) I want to be able to sum all the numbers in a group.
+	2) I want to be able to calculate the mean of a group of numbers.
+	3) I want to be able to calculate the median of a group of numbers.
